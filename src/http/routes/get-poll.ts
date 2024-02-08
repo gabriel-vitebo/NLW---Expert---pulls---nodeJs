8e4756b0fc13d0445ad4a9639d82d572e81cacd4
gabriel-vitebo <vitebo@outlook.com>
2024-02-08 16:44:16 -0300

    create get-poll route

---
+++ src/http/routes/get-poll.ts
@@ -0,0 +1,30 @@
+import z from "zod"
+import { app } from "../app"
+import { prisma } from "../../lib/prisma"
+
+export async function getPoll() {
+  app.get('/polls/:pollId', async (request, reply) => {
+    const getPollParams = z.object({
+      pollId: z.string().uuid()
+    })
+
+    const { pollId } = getPollParams.parse(request.params)
+
+    const poll = await prisma.poll.findUnique({
+      where: {
+        id: pollId
+      },
+      include: {
+        options: {
+          select: {
+            id: true,
+            title: true,
+          }
+        }
+      }
+    })
+
+    return reply.send({ poll })
+  })
+
+}
--- src/http/server.ts
+++ src/http/server.ts
@@ -1,7 +1,9 @@
 import { app } from "./app";
 import { createPoll } from "./routes/create-poll";
+import { getPoll } from "./routes/get-poll";
 
 app.register(createPoll)
+app.register(getPoll)
 
 app.listen({ port: 3333 }).then(() => {
   console.log('HTTP Server Running on Port 3333')
