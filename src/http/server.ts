9c2046fb481c104d414097a07180db8ca85dfb9f
gabriel-vitebo <vitebo@outlook.com>
2024-02-08 16:49:37 -0300

    create vote-on-poll routes

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,9 +1,11 @@
 import { app } from "./app";
 import { createPoll } from "./routes/create-poll";
 import { getPoll } from "./routes/get-poll";
+import { voteOnPoll } from "./routes/vote-on-poll";
 
 app.register(createPoll)
 app.register(getPoll)
+app.register(voteOnPoll)
 
 app.listen({ port: 3333 }).then(() => {
   console.log('HTTP Server Running on Port 3333')
