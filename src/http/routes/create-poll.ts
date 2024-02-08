52a474e3b7946615578ac898d359f4bb707856d1
gabriel-vitebo <vitebo@outlook.com>
2024-02-08 13:41:22 -0300

    fix prisma bug

--- src/http/routes/create-poll.ts
+++ src/http/routes/create-poll.ts
@@ -1,18 +1,26 @@
 import z from "zod"
-import { prisma } from "../../lib/prisma"
 import { app } from "../app"
+import { prisma } from "../../lib/prisma"
 
 export async function createPoll() {
   app.post('/polls', async (request, reply) => {
     const createPollBody = z.object({
-      title: z.string()
+      title: z.string(),
+      options: z.array(z.string())
     })
 
-    const { title } = createPollBody.parse(request.body)
+    const { title, options } = createPollBody.parse(request.body)
 
     const poll = await prisma.poll.create({
       data: {
         title,
+        options: {
+          createMany: {
+            data: options.map((option) => {
+              return { title: option }
+            }),
+          }
+        }
       }
     })
 
